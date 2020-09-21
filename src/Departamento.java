f4d6a00b0ca9ddb8c2edf98172344cc5e65e6879
Alex lopes rocha <alex.rocha@ufvjm.edu.br>
2020-09-21 10:51:03 -0300

    commit inicial

---
+++ src/Departamento.java
@@ -0,0 +1,56 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ *
+ * @author SI04
+ */
+public class Departamento {
+    
+    private int codigo;
+    private String nome;
+    private List<Funcionario> listaFunc;
+
+    public Departamento() {
+        listaFunc = new ArrayList();
+    }
+
+    public Departamento(int codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+        listaFunc = new ArrayList();
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Funcionario> getListaFunc() {
+        return listaFunc;
+    }
+
+    public void setListaFunc(List<Funcionario> listaFunc) {
+        this.listaFunc = listaFunc;
+    }
+    
+    public void addFunc(Funcionario func){
+        func.setDep(this);
+        listaFunc.add(func);
+    }
+    
+}
---
+++ src/Funcionario.java
@@ -0,0 +1,44 @@
+/**
+ *
+ * @author SI04
+ */
+public class Funcionario {
+    
+    private int matricula;
+    private String nome;
+    private Departamento dep;
+
+    public Funcionario(int matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public Funcionario() {
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Departamento getDep() {
+        return dep;
+    }
+
+    public void setDep(Departamento dep) {
+        this.dep = dep;
+    }
+    
+    
+}
